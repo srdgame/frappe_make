ffe23653bdc153c048cf537b4f2d72c06c2516a7
Dirk Chang <dirk@kooiot.com>
2017-05-21 17:07:50 +0800

    Batch Create Button

--- make/make/doctype/make_batch/make_batch.js
+++ make/make/doctype/make_batch/make_batch.js
@@ -2,7 +2,44 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Make Batch', {
+	setup: function(frm) {
+	},
 	refresh: function(frm) {
+		me = this;
+		frm.add_custom_button(__("Create Batch Serial NO"), function() {
+			me.batch_create = new frappe.ui.Dialog({
+				'title': 'Batch Create',
+				fields: [
+					{
+						"label": __("Count of Serial NO"),
+						"fieldname": "count",
+						"fieldtype": "Int",
+						"reqd": 1
+					}
+				]
+			});
 
+			me.batch_create.show();
+			me.batch_create.set_primary_action(__("Create"), function () {
+				frm.events.batch_create(frm, me.batch_create.get_values().count);
+			});
+		}).removeClass("btn-default").addClass("btn-primary");
+	},
+	batch_create: function(frm, count) {
+		return frappe.call({
+			doc: frm.doc,
+			method: "batch_create",
+			args: {"count": count},
+			freeze: true,
+			callback: function(r) {
+				me.batch_create.hide();
+				if(!r.exc) {
+					frm.refresh_fields();
+					msgprint(r.message);
+				} else {
+					msgprint(r._server_message);
+				}
+			}
+		})
 	}
 });
--- make/make/doctype/make_batch/make_batch.py
+++ make/make/doctype/make_batch/make_batch.py
@@ -13,3 +13,12 @@ class MakeBatch(Document):
 			throw(_("Batch NO length must be six!"))
 		serial_code = frappe.get_value("Make Item", self.item, "serial_code")
 		self.serial_code = "{0}-{1}".format(serial_code, self.batch_no)
+
+	def batch_create(self, count):
+		for i in range(count):
+			doc = frappe.get_doc({
+				"doctype": "Make Serial NO",
+				"item": self.item,
+				"batch": self.name,
+			}).insert()
+		return "{0} Serial NO has been created!".format(count)
