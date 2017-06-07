3c719cb6b6b1c55cf5f0f7e2efb6a66ca66f3360
Dirk Chang <dirk@kooiot.com>
2017-06-07 18:55:40 +0800

    Translation

--- make/license/doctype/device_license_bundle/device_license_bundle.py
+++ make/license/doctype/device_license_bundle/device_license_bundle.py
@@ -17,7 +17,6 @@ class DeviceLicenseBundle(Document):
 					throw(_("Device SN {0} is licensed by {1}").format(dev.sn, sid))
 
 	def on_update(self):
-		type_value = frappe.get_value("Device License Type", self.type, "value")
 		sn_list = [d.sn for d in self.devices]
 		for d in frappe.db.get_values("Device License", {"source_id": self.name}, "sn"):
 			if d[0] not in sn_list:
@@ -25,7 +24,6 @@ class DeviceLicenseBundle(Document):
 			else:
 				frappe.set_value("Device License", d[0], "expire_date", self.expire_date)
 				frappe.set_value("Device License", d[0], "type", self.type)
-				frappe.set_value("Device License", d[0], "type_value", type_value)
 				frappe.set_value("Device License", d[0], "enabled", self.enabled)
 
 		for dev in self.devices:
@@ -34,7 +32,6 @@ class DeviceLicenseBundle(Document):
 					"doctype": "Device License",
 					"sn": dev.sn,
 					"type": self.type,
-					"type_value": type_value,
 					"enabled": self.enabled,
 					"expire_date": self.expire_date,
 					"source_type": self.doctype,
