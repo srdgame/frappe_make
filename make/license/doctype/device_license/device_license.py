c2a0ed6b5713c265b3dbd83d7158f97c1dd29812
Dirk Chang <dirk@kooiot.com>
2017-06-15 16:35:18 +0800

    License data update

--- make/license/doctype/device_license/device_license.py
+++ make/license/doctype/device_license/device_license.py
@@ -4,9 +4,46 @@
 
 from __future__ import unicode_literals
 import frappe
+import requests
 from frappe.model.document import Document
 
 
 class DeviceLicense(Document):
 	def validate(self):
-		self.license_data = str(self.expire_date) + " AAAA"
+		if not self.source_id:
+			self.license_need_update = 1
+
+	def on_update(self):
+		if self.license_need_update == 1:
+			frappe.enqueue('make.license.doctype.device_license.device_license.get_license_data',
+							doc_name=self.name, doc_doc=self)
+
+	def update_license_data(self):
+		url = frappe.db.get_single_value("Device License Settings", "server_url")
+		username = frappe.db.get_single_value("Device License Settings", "username")
+		passwd = frappe.db.get_single_value("Device License Settings", "password")
+		session = requests.session()
+		session.auth = (username, passwd)
+		session.headers['Content-Type'] = 'application/json'
+		session.headers['Accept'] = 'application/json'
+		type_doc = frappe.get_doc('Device License Type', self.type)
+		r = session.post(url, data= {
+			'type': type_doc.get_type(),
+			'devices': [{
+				'sn': self.sn,
+				'pcid': 'from_web',
+				'mac': '',
+			}]
+		})
+		self.set('license_data', r.json()[self.sn])
+
+
+def get_license_data(doc_name, doc_doc=None):
+	doc = doc_doc or frappe.get_doc("Device License", doc_name)
+	return doc.update_license_data()
+
+
+def license_update():
+	for doc in frappe.get_all("Device License", "name", filters={"license_need_update": 1}):
+		frappe.enqueue('make.license.doctype.device_license.device_license.get_license_data',
+						doc_name=doc.name)
--- make/license/doctype/device_license_bundle/device_license_bundle.py
+++ make/license/doctype/device_license_bundle/device_license_bundle.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe
+import requests
 from frappe import throw, _
 from frappe.model.document import Document
 
@@ -38,7 +39,45 @@ class DeviceLicenseBundle(Document):
 					"source_id": self.name
 				}).insert()
 
+		frappe.enqueue('make.license.doctype.device_license_bundle.device_license_bundle.get_license_data',
+						doc_name=self.name, doc_doc=self)
+
 	def on_trash(self):
 		for dev in self.devices:
 			frappe.delete_doc("Device License", dev.sn)
 
+	def update_license_data(self):
+		url = frappe.db.get_single_value("Device License Settings", "server_url")
+		username = frappe.db.get_single_value("Device License Settings", "username")
+		passwd = frappe.db.get_single_value("Device License Settings", "password")
+		session = requests.session()
+		session.auth = (username, passwd)
+		session.headers['Content-Type'] = 'application/json'
+		session.headers['Accept'] = 'application/json'
+		type_doc = frappe.get_doc('Device License Type', self.type)
+		devices = []
+		for dev in self.devices:
+			devices.append({
+				'sn': self.sn,
+				'pcid': 'from_web',
+				'mac': '',
+			})
+		r = session.post(url, data= {
+			'type': type_doc.get_type(),
+			'devices': devices
+		})
+		for dev in self.devices:
+			doc = frappe.get_doc('Device License', dev.sn)
+			doc.set('license_data', r.json()[self.sn])
+			doc.save()
+
+
+def get_license_data(doc_name, doc_doc=None):
+	doc = doc_doc or frappe.get_doc("Device License Bundle", doc_name)
+	return doc.update_license_data()
+#
+#
+# def license_update():
+# 	for doc in frappe.get_all("Device License Bundle", "name", filters={"license_need_update": 1}):
+# 		frappe.enqueue('make.license.doctype.device_license_bundle.device_license_bundle.get_license_data',
+# 						doc_name=doc.name)
